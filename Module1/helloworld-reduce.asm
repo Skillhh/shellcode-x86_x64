4573b628e9a825c43ce6112f3223ca309c013072
Skillhh <ortegon.giovanny@hotmail.com>
2020-07-29 18:25:26 -0500

    Revert "[Error]"
    This reverts commit cc2ab1b1758f019758cc0e166d15e2d0e7560454.

---
+++ Module1/helloworld-reduce.asm
@@ -0,0 +1,19 @@
+global _start
+
+section .text
+_start:
+	; print on screen
+	mov al, 1
+	mov dil, 1
+	mov esi, msg
+	mov dl, len
+	syscall 
+
+	; exit gracefully
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .data
+	msg: db 'Hello World to the SLAE-64 Course', 0xa
+	len: equ $ - msg
